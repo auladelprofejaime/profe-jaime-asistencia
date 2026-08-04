f15d4b52dbbd310fd08e8f2d2216798100210be2
auladelprofejaime <auladelprofejaime@gmail.com>
2026-08-03 21:31:35 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE='aula-profe-jaime-v5-9-0';
+const CACHE='aula-profe-jaime-v6-0-0';
 const LOCAL=[
- './','./index.html','./styles.css','./app-v59.js?v=590',
+ './','./index.html','./styles.css','./app-v60.js?v=600',
  './manifest.webmanifest','./icon.svg','./avatar-profe-jaime.png','./icon-profe-jaime.png'
 ];
 const OPTIONAL=[

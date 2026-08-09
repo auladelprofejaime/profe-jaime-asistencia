ddf840566b22ca5a16376719fde9abb08d4489c2
auladelprofejaime <auladelprofejaime@gmail.com>
2026-08-09 12:12:41 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE='app-docente-v8-9-8';
+const CACHE='app-docente-v8-9-9';
 const LOCAL=[
- './','./index.html','./styles.css','./app-v898.js?v=898',
+ './','./index.html','./styles.css','./app-v899.js?v=899',
  './manifest.webmanifest','./icon.svg','./avatar-profe-jaime.png','./icon-app-docente-v821.png',
  './shared/supabase-teacher.js?v=790'
 ];

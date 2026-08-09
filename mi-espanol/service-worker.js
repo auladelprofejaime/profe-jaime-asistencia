64d45ac3ea31b374b69d486e67e1e343bd2f7e74
auladelprofejaime <auladelprofejaime@gmail.com>
2026-08-08 20:43:11 -0600

    Add files via upload

--- mi-espanol/service-worker.js
+++ mi-espanol/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE='app-estudiante-v8-9';
+const CACHE='app-estudiante-v8-9-2';
 const FILES=[
   './',
   './index.html',
   './styles.css',
-  './app.js?v=890',
+  './app.js?v=892',
   './manifest.webmanifest',
   './profe-jaime.png',
   './icon-app-estudiante-v821.png',

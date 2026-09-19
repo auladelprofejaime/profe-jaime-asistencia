a395c17f05e4c4c66d53e5fe2cb77cb90ed8a365
auladelprofejaime <auladelprofejaime@gmail.com>
2026-09-19 16:53:16 -0600

    App Docente v8.23.18: actualiza caché

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE='app-docente-v8-23-17';
+const CACHE='app-docente-v8-23-18';
 const APP_CACHE_PREFIX='app-docente-';
 const ROOT_PATH=new URL('./',self.location.href).pathname;
 const LOCAL=[
- './','./index.html','./styles.css','./app-v82312.js?v=82312','./merit-period-fix-v82313.js?v=82313','./merit-schoolwide-fix-v82314.js?v=82314','./merit-staff-provision-v82317.js?v=82317',
+ './','./index.html','./styles.css','./app-v82312.js?v=82312','./merit-period-fix-v82313.js?v=82313','./merit-schoolwide-fix-v82314.js?v=82314','./merit-staff-provision-v82318.js?v=82318',
  './manifest.webmanifest','./icon.svg','./avatar-profe-jaime.png','./icon-app-docente-v821.png','./logo-app-docente-v821.png',
  './shared/supabase-teacher.js?v=800'
 ];

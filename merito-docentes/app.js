5fa4f868c454fe08fd9417470c06f3aff4685cd1
auladelprofejaime <auladelprofejaime@gmail.com>
2026-09-19 20:26:12 -0600

    Mérito Docentes v29: corte duro al final del periodo

--- merito-docentes/app.js
+++ merito-docentes/app.js
@@ -559,6 +559,6 @@ $('#movementReviewSend')?.addEventListener('click',async()=>{
 
 $('#enableNotificationsBtn')?.addEventListener('click',enableMeritNotifications);
 if($('#rememberSession'))$('#rememberSession').checked=rememberSession||(!rememberedToken&&!sessionToken);
-if('serviceWorker'in navigator)navigator.serviceWorker.register('service-worker.js?v=28').catch(()=>{});
+if('serviceWorker'in navigator)navigator.serviceWorker.register('service-worker.js?v=29').catch(()=>{});
 updateOfflineUI();
 checkDevice();

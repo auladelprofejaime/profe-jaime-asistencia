5525a721df93ee7fc7edd7812133bf122543b50f
auladelprofejaime <auladelprofejaime@gmail.com>
2026-09-20 16:59:22 -0600

    Renueva caché App Docente v8.23.37

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='app-docente-v8-23-36';
+const CACHE='app-docente-v8-23-37';
 const APP_CACHE_PREFIX='app-docente-';
 const ROOT_PATH=new URL('./',self.location.href).pathname;
 const LOCAL=[

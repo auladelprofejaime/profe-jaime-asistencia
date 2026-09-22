49f86d74897ffcf8f5e70a978b6ade8cc41b2239
auladelprofejaime <auladelprofejaime@gmail.com>
2026-09-22 07:17:39 -0600

    Bump Merit docentes service worker registration

--- merito-docentes/app.js
+++ merito-docentes/app.js
@@ -577,6 +577,6 @@ $('#movementReviewSend')?.addEventListener('click',async()=>{
 
 $('#enableNotificationsBtn')?.addEventListener('click',enableMeritNotifications);
 if($('#rememberSession'))$('#rememberSession').checked=rememberSession||(!rememberedToken&&!sessionToken);
-if('serviceWorker'in navigator)navigator.serviceWorker.register('service-worker.js?v=31').catch(()=>{});
+if('serviceWorker'in navigator)navigator.serviceWorker.register('service-worker.js?v=32').catch(()=>{});
 updateOfflineUI();
 checkDevice();
